ed4dec58d88588924cc3f597e00f53b05cbe554d
ChristianJaimes <christiandja@gmail.com>
2024-10-01 23:00:39 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='retry_ops',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python library providing retry decorators.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
